05c06691ac61a53520c8c533345d413e13a18938
Jakub Binkowski <jakubin@gmail.com>
2017-06-24 10:22:07 +0200

    Dummy FibonacciController

---
+++ src/Fibon.Api/Controllers/FibonacciController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fibon.Api.Controllers
+{
+    [Route("[controller]")]
+    public class FibonacciController: Controller
+    {
+        [HttpGet("{number}")]
+        public async Task<IActionResult> Get(int number)
+        {
+            return Content(number.ToString());
+        }
+
+        [HttpPost("{number}")]
+        public async Task<IActionResult> Post(int number)
+        {
+            return Accepted($"fibonacci/{number}");
+        }
+    }
+}
